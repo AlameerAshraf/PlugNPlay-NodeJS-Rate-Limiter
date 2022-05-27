0ac2bf0b4f9145b5553b8396521cd72ca922a131
alameerashraf <alamiir.ashraf@gmail.com>
2022-05-28 01:52:02 +0200

    Run redis from docker

--- index.js
+++ index.js
@@ -8,20 +8,20 @@ const port = process.env.PORT;
 
 
 
-// const client = redis.createClient({
-//     host: 'localhost',
-//     port: '6479',
-// });
+const client = redis.createClient({
+    host: '127.0.0.1', //redis the name of the servise in docker compose 
+    port: '6379',
+});
 
-// await client.connect();
+await client.connect();
 
-// client.set("user" , 23);
+client.set("user" , 23);
 
-// let res = await client.get("user");
+let res = await client.get("user");
 
-// console.log("we" ,res)
+console.log("we" ,res)
 
-// console.log(client);
+console.log(client);
 
 app.listen(port, () => {
     console.log("Rate Limiter Server is Up and Running on  " + port);
