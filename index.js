401e448580f360c493396a2a9c4a0b6df7b63086
alameerashraf <alamiir.ashraf@gmail.com>
2022-05-28 01:32:11 +0200

    Adding redis to the utilities

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 import express from 'express';
+import redis from 'redis';
 import 'dotenv/config';
 
 const app = express();
@@ -7,7 +8,20 @@ const port = process.env.PORT;
 
 
 
+// const client = redis.createClient({
+//     host: 'localhost',
+//     port: '6479',
+// });
 
+// await client.connect();
+
+// client.set("user" , 23);
+
+// let res = await client.get("user");
+
+// console.log("we" ,res)
+
+// console.log(client);
 
 app.listen(port, () => {
     console.log("Rate Limiter Server is Up and Running on  " + port);
